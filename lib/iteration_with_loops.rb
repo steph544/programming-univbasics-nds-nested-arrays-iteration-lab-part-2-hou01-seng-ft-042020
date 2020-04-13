a6965079ccc43242d2c6194b18b5bdb02e2e9761
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-13 15:40:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -17,3 +17,21 @@ require 'pry'
 # end 
 # new_array 
 # end
+
+find_greater_pair (array)
+new_array=[]
+counter=0 
+while counter < array.length do 
+  element_index=0 
+  while element_index<array[counter].length do 
+  if array[counter][element_index] > array[counter][element_index +1]
+    new_array << array[counter][element_index]
+  elsif array[counter][element_index +1] > array[counter][element_index]
+    new_array << array[counter][element_index +1]
+  end 
+  element_index+=1 
+end 
+counter+=1 
+new_array
+end 
+  
