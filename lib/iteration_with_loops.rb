cfe0d69deadc9649a1f2574e15707c3c1502b505
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-13 15:35:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,19 +1,19 @@
 require 'pry'
 
-def find_min_in_nested_arrays(src)
-  new_array=[]
-  counter=0 
-  while counter<src.length do 
-    element_index=0 
-    while element_index < src[counter].length  do 
-      value=src[counter][element_index] 
-      if src[counter][element_index += 1] < value 
-         value= src[counter][element_index += 1]
-      end 
-     element_index+=1 
-   end 
-   new_array << value 
-  counter+=1 
-end 
-new_array 
-end
+# def find_min_in_nested_arrays(src)
+#   new_array=[]
+#   counter=0 
+#   while counter<src.length do 
+#     element_index=0 
+#     while element_index < src[counter].length  do 
+#       value=src[counter][element_index] 
+#       if src[counter][element_index += 1] < value 
+#         value= src[counter][element_index += 1]
+#       end 
+#     element_index+=1 
+#   end 
+#   new_array << value 
+#   counter+=1 
+# end 
+# new_array 
+# end
